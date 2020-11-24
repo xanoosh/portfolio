bd3cf5739c4391d992a60c7fca55a8d7bb7f9ae5
xanoosh <xanoosh@gmail.com>
2020-11-24 22:04:41 +0100

    added popup instructions to pig game and some minor css js changes

--- modules/dice-content/script.js
+++ modules/dice-content/script.js
@@ -11,80 +11,105 @@ const playerTwoScore = document.getElementById('current--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnHow = document.querySelector('.btn--how');
+const btnClosePopup = document.querySelector('.close-popup');
+const overlay = document.querySelector('.overlay');
 
 let currentScore = 0;
 let activePlayer = 0;
-const scores = [0,0];
+const scores = [0, 0];
 
 //function player switcher
 
-const switchPlayer = function(){
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    diceEl.classList.add('hidden');
-}
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(
+    `current--${activePlayer}`
+  ).textContent = currentScore;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  //diceEl.classList.add('hidden');
+};
 
 //roll dice function
-const rollDice = function(){
-    //random number 1-6
-    let random = Math.trunc(Math.random()*6) + 1;
-    let imgSrc = `modules/dice-content/dice-${random}.png`
-    if (diceEl.classList.contains('hidden')){
-        diceEl.classList.remove('hidden');
-    }
-    diceEl.src = imgSrc;
-    //add random roll to current score
-    if(random !== 1){
-        currentScore += random;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    }else{
-        switchPlayer();
-    }
-}
+const rollDice = function () {
+  //random number 1-6
+  let random = Math.trunc(Math.random() * 6) + 1;
+  let imgSrc = `modules/dice-content/dice-${random}.png`;
+  if (diceEl.classList.contains('hidden')) {
+    diceEl.classList.remove('hidden');
+  }
+  diceEl.src = imgSrc;
+  //add random roll to current score
+  if (random !== 1) {
+    currentScore += random;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    switchPlayer();
+  }
+};
 
 //score hold function
-const holdScore = function(){
-    //check score value
-    let scoreVal = document.getElementById(`score--${activePlayer}`);
-    let summaryValue = Number(scoreVal.textContent) + currentScore;
+const holdScore = function () {
+  //check score value
+  let scoreVal = document.getElementById(`score--${activePlayer}`);
+  let summaryValue = Number(scoreVal.textContent) + currentScore;
 
-    if(summaryValue < 100){
-        //console.log(scoreVal.textContent);
-        scoreVal.textContent = Number(scoreVal.textContent) + currentScore;
-        switchPlayer();
-    }
-    else{
-       // summaryValue = 100;
-        scoreVal.textContent = 100;
-        console.log(`Player ${activePlayer + 1} has won the game!`);
-        document.querySelector('main').classList.add('winner');
-        document.querySelector('.who-won').textContent = `Player ${activePlayer + 1} has won the game!`;
-    }
-}
-
-const resetValues = function(){
-    currentScore = 0;
-    activePlayer = 0;
-    scorePlayer0El.textContent = 0
-    scorePlayer1El.textContent = 0;
-    document.getElementById(`current--1`).textContent = 0;
-    document.getElementById(`current--0`).textContent = 0;
+  if (summaryValue < 100) {
+    //console.log(scoreVal.textContent);
+    scoreVal.textContent = Number(scoreVal.textContent) + currentScore;
+    switchPlayer();
     diceEl.classList.add('hidden');
-    document.querySelector('main').classList.remove('winner');
+  } else {
+    // summaryValue = 100;
+    scoreVal.textContent = Number(scoreVal.textContent) + currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document.getElementById(`name--${activePlayer}`).textContent = 'Winner!';
+    btnRoll.style.pointerEvents = 'none';
+    btnHold.style.pointerEvents = 'none';
+    btnNew.style.backgroundColor = '#fff';
+  }
+};
 
-}
+//reset values for new game
+const resetValues = function () {
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.getElementById(
+    `name--${activePlayer}`
+  ).textContent = `Player ${activePlayer}`;
+  currentScore = 0;
+  activePlayer = 0;
+  scorePlayer0El.textContent = 0;
+  scorePlayer1El.textContent = 0;
+  document.getElementById(`current--1`).textContent = 0;
+  document.getElementById(`current--0`).textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  btnRoll.style.pointerEvents = 'all';
+  btnHold.style.pointerEvents = 'all';
+  btnNew.style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
+};
 
-//score0El.textContent = 0;
-//score1El.textContent = 0;
-//diceEl.classList.add('hidden');
+//popup mechanics
+btnHow.addEventListener('click', function () {
+  overlay.classList.remove('opacity');
+});
+overlay.addEventListener('click', function () {
+  overlay.classList.add('opacity');
+});
+btnClosePopup.addEventListener('click', function () {
+  overlay.classList.add('opacity');
+});
 
 btnRoll.addEventListener('click', rollDice);
 btnHold.addEventListener('click', holdScore);
 btnNew.addEventListener('click', resetValues);
-
-
-
-
