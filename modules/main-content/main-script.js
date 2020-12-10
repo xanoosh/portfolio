6f0db894cde3b4845bef736864e5fa0bdc5665ae
xanoosh <xanoosh@gmail.com>
2020-12-11 00:53:21 +0100

    more main sections small aos experiment

--- modules/main-content/main-script.js
+++ modules/main-content/main-script.js
@@ -1,2 +1,9 @@
 'use strict';
+
 AOS.init();
+
+let delayVal = 0;
+document.querySelectorAll(`[data-aos='fade-up']`).forEach((el) => {
+  el.setAttribute('data-aos-delay', delayVal);
+  delayVal += 100;
+});
