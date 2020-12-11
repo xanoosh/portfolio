382300b8eb18207a6ba0d9a4b9a9d41cfd0fd0fb
xanoosh <xanoosh@gmail.com>
2020-12-12 00:40:44 +0100

    gave up fr now, added typed.js library

--- modules/main-content/main-script.js
+++ modules/main-content/main-script.js
@@ -1,42 +1,24 @@
 'use strict';
 
+//AOS setup
 AOS.init();
-
 let delayVal = 0;
 document.querySelectorAll(`[data-aos='fade-up']`).forEach((el) => {
   el.setAttribute('data-aos-delay', delayVal);
   delayVal += 100;
 });
 
+//type.js setup
+const options = {
+  strings: [
+    'Hello there!^2000',
+    'I am a web developer from Warsaw^1500',
+    'Hope you enjoy my projects :)',
+  ],
+  typeSpeed: 60,
+};
+const typed = new Typed('.type-it', options);
+
 // get some tinytyper / typer.js - typing animation
 //...or write your own
 //try to write your own animate on scroll :)
-
-// const typeTxt = document.querySelector('.type-it');
-// // const txt = 'Hello  there! ssss';
-// let i = 0;
-// typeTxt;
-
-// //explode to words
-// const typeWrite = function (fullTxt) {
-//   const words = fullTxt.split(' ');
-//   words.forEach((word) => {
-//     console.log(word);
-//     if (word.length > 0) {
-//       typer(word);
-//     }
-//   });
-//   i = 0;
-// };
-
-// // define typer for single word
-// const typer = function (txt) {
-//   if (i < txt.length) {
-//     // console.log(txt + 'again' + typeof txt);
-//     typeTxt.innerText += txt.charAt(i);
-//     i++;
-//     setTimeout(typer, 50);
-//   }
-// };
-
-// setTimeout(typeWrite('Hello  there! ssss'), 500);
