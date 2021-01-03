97e16f9e17f3573bf455acd2f801e1dfb8d235a3
xanoosh <xanoosh@gmail.com>
2021-01-03 13:33:45 +0100

    added product list genterationg

--- modules/recipe-api-content/recipe-api-script.js
+++ modules/recipe-api-content/recipe-api-script.js
@@ -25,5 +25,27 @@ const fetchApi = () => {
   request();
 };
 
-//get recipe
-init.addEventListener('click', fetchApi);
+const createList = async () => {
+  ingredientsArr.forEach((element) => {
+    const image = document.createElement('img');
+    image.src = element.image;
+    const heading = document.createElement('h3');
+    heading.innerText = element.title;
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = element.id;
+    const container = document.createElement('div');
+    container.classList.add('single');
+    main.appendChild(container);
+    container.appendChild(image);
+    container.appendChild(heading);
+    container.appendChild(paragraph);
+  });
+};
+
+function initialize() {
+  fetchApi();
+  createList();
+}
+
+//onclick get recipe
+init.addEventListener('click', initialize);
