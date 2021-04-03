93eab66760c6a50a196d44c88464878808e37775
xanoosh <xanoosh@gmail.com>
2021-04-03 12:05:43 +0200

    snake start function on keypress

--- snake/snake-script.js
+++ snake/snake-script.js
@@ -2,9 +2,22 @@
 console.log('snake');
 
 const board = document.getElementById('board');
+//38, 39, 40, 37
+//up right down left
 
-for (let i = 0; i < 25; i++) {
-  const cell = document.createElement('div');
-  cell.classList.add('cell');
-  board.appendChild(cell);
-}
+const gameLoop = (e) => {
+  if (e.keyCode === 38) {
+    console.log('up');
+  }
+  if (e.keyCode === 39) {
+    console.log('right');
+  }
+  if (e.keyCode === 40) {
+    console.log('down');
+  }
+  if (e.keyCode === 37) {
+    console.log('left');
+  }
+};
+
+document.addEventListener('keyup', (e) => gameLoop(e));
