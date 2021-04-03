fad8f4c943e914c6dc8fcc741592029f138d8aa3
xanoosh <xanoosh@gmail.com>
2021-04-03 12:55:51 +0200

    snake and food object with moving method added

--- snake/snake-script.js
+++ snake/snake-script.js
@@ -2,12 +2,62 @@
 console.log('snake');
 
 const board = document.getElementById('board');
-//38, 39, 40, 37
-//up right down left
+const snakeSpeed = 2;
+//moves per second
+
+let snakePosition = [{ x: 17, y: 14 }];
+let loopEvent = '';
+
+const snake = {
+  position: [{ x: 17, y: 14 }],
+  move: function (keyCode) {
+    if (keyCode === 38) {
+      this.position[0].x -= 1;
+    }
+    if (keyCode === 40) {
+      this.position[0].x += 1;
+    }
+    if (keyCode === 39) {
+      this.position[0].y += 1;
+    }
+    if (keyCode === 37) {
+      this.position[0].y -= 1;
+    }
+  },
+};
+const food = {
+  position: { x: 10, y: 10 },
+};
+
+const getPosition = () => {
+  // if (snakeElement !== undefined) {
+  //   snakeElement.remove();
+  // }
+  const snakeElement = document.createElement('div');
+  snakeElement.style.gridRowStart = snake.position[0].x;
+  snakeElement.style.gridColumnStart = snake.position[0].y;
+  snakeElement.classList.add('snake');
+  board.appendChild(snakeElement);
+  const foodElement = document.createElement('div');
+  foodElement.style.gridRowStart = food.position.x;
+  foodElement.style.gridColumnStart = food.position.y;
+  foodElement.classList.add('food');
+  board.appendChild(foodElement);
+};
+
+getPosition();
 
 const gameLoop = (e) => {
+  window.clearInterval(loopEvent);
+  snake.move(e.keyCode);
+  console.log(snake.position[0]);
+  getPosition();
+  // snake.position;
   if (e.keyCode === 38) {
     console.log('up');
+    // loopEvent = window.setInterval(() => {
+    //   console.log('up');
+    // }, 1000 / snakeSpeed);
   }
   if (e.keyCode === 39) {
     console.log('right');
