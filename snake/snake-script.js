41253739386a9bf729f0b03112a0c0363dfa5ee0
xanoosh <xanoosh@gmail.com>
2021-04-04 17:23:58 +0200

    portfolio snake eat function

--- snake/snake-script.js
+++ snake/snake-script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const board = document.getElementById('board');
+let food = '';
 const snakeSpeed = 4;
 //moves per second
 let loopEvent = '';
@@ -15,6 +16,24 @@ const snake = {
   ],
   prevPosition: { x: 0, y: 0 },
   newPosition: { x: 0, y: 0 },
+  food: {
+    position: { x: 12, y: 12 },
+    placeFood: function () {},
+  },
+
+  eat: function () {
+    if (
+      this.food.position.x === this.newPosition.x &&
+      this.food.position.y === this.newPosition.y
+    ) {
+      //eat that shit
+      food = document.getElementById('food');
+      console.log(food);
+      food.classList.add('eaten');
+
+      // eaten.classList.remove('.food');
+    }
+  },
 
   updatePosition: function () {
     this.position.map((el) => {
@@ -29,11 +48,11 @@ const snake = {
   moveLoop: function (keyCode) {
     if (keyCode === 38) {
       loopEvent = setInterval(() => {
-        if (this.gameLost) this.gameLost();
         this.newPosition = {
           x: this.position[0].x - 1,
           y: this.position[0].y,
         };
+        this.eat();
         this.gameLost();
         if (!this.gameLost()) {
           this.updatePosition();
@@ -42,11 +61,11 @@ const snake = {
     }
     if (keyCode === 40) {
       loopEvent = setInterval(() => {
-        if (this.gameLost) this.gameLost();
         this.newPosition = {
           x: this.position[0].x + 1,
           y: this.position[0].y,
         };
+        this.eat();
         this.gameLost();
         if (!this.gameLost()) {
           this.updatePosition();
@@ -59,20 +78,22 @@ const snake = {
           x: this.position[0].x,
           y: this.position[0].y + 1,
         };
+        this.eat();
         this.gameLost();
-        // if (!this.gameLost()) {
-        this.updatePosition();
+        if (!this.gameLost()) {
+          this.updatePosition();
+        }
         // }
       }, 1000 / snakeSpeed);
     }
     if (keyCode === 37) {
       loopEvent = setInterval(() => {
-        if (this.gameLost) this.gameLost();
         this.newPosition = {
           x: this.position[0].x,
           y: this.position[0].y - 1,
         };
-
+        this.eat();
+        this.gameLost();
         if (!this.gameLost()) {
           this.updatePosition();
         }
@@ -102,9 +123,6 @@ const snake = {
     });
   },
 };
-const food = {
-  position: { x: 12, y: 12 },
-};
 
 const removeCurrentSnake = () => {
   const currentSnakeSegments = document.querySelectorAll('.snake');
@@ -123,9 +141,9 @@ const renderSnake = () => {
     board.appendChild(snakeElement);
   });
   const foodElement = document.createElement('div');
-  foodElement.style.gridRowStart = food.position.x;
-  foodElement.style.gridColumnStart = food.position.y;
-  foodElement.classList.add('food');
+  foodElement.style.gridRowStart = snake.food.position.x;
+  foodElement.style.gridColumnStart = snake.food.position.y;
+  foodElement.id = 'food';
   board.appendChild(foodElement);
 };
 
