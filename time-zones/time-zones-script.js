e019e1c41b5ad7313b73d8c6ed2122a56c75a886
xanoosh <xanoosh@gmail.com>
2021-04-10 20:32:09 +0200

    time zones project wokring on unified calculation

--- time-zones/time-zones-script.js
+++ time-zones/time-zones-script.js
@@ -6,6 +6,7 @@ const inputNodes = document.querySelectorAll("input[type='time']");
 const key = 'ed8962ccee5b4be0a8ed091664951800';
 let tokioPrev;
 let berlinPrev;
+let moscowPrev;
 
 function hourToString(date) {
   function addZero(num) {
@@ -36,10 +37,11 @@ function getTime(node, location) {
       const time = result.time_24.slice(0, 5);
       if (node.name === 'Berlin') berlinPrev = time;
       if (node.name === 'Tokio') tokioPrev = time;
+      if (node.name === 'Moscow') moscowPrev = time;
       const date = new Date(result.date_time_txt);
       node.value = time;
       node.addEventListener('change', () => {
-        changeInput(date);
+        changeInput(node);
       });
       return;
     });
@@ -49,10 +51,13 @@ inputNodes.forEach((node) => {
   getTime(node, node.name);
 });
 
-function changeInput() {
+function changeInput(node) {
+  const [hours, minutes] = node.value.split(':');
   console.log('change');
+  if (node.name === 'Moscow');
+  const newHour = new Date().setHours(hours, minutes);
+  console.log(newHour);
 }
-function updateTime() {}
 
 // inputNodes.forEach((node) => {
 //   const date = getTime(node, node.name);
