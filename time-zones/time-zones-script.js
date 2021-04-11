5d853a906acc02c7459228d4d1c250acfab0b8a7
xanoosh <xanoosh@gmail.com>
2021-04-11 19:03:54 +0200

    time zones project - rewriting to oop

--- time-zones/time-zones-script.js
+++ time-zones/time-zones-script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const timeCurrent = document.getElementById('time-current');
-const date = new Date();
+const cities = new Set([]);
 const inputNodes = document.querySelectorAll("input[type='time']");
 const key = 'ed8962ccee5b4be0a8ed091664951800';
 let tokioPrev;
@@ -9,62 +9,56 @@ let berlinPrev;
 let delhiPrev;
 let newYorkPrev;
 
-function getPreviousValue(txt) {
-  if (txt === 'Tokio') return tokioPrev;
-  if (txt === 'Berlin') return berlinPrev;
-  if (txt === 'Delhi') return delhiPrev;
-  if (txt === 'New York') return newYorkPrev;
-}
-function updatePreviousValue(txt, val) {
-  if (txt === 'Tokio') tokioPrev = val;
-  if (txt === 'Berlin') berlinPrev = val;
-  if (txt === 'Delhi') delhiPrev = val;
-  if (txt === 'New York') newYorkPrev = val;
-}
-
-function hourToString(input) {
-  const date = new Date(input);
-  function addZero(num) {
-    if (num < 10) return '0' + num;
-    return num;
+class TimeZone {
+  constructor(name) {
+    this.name = name;
+    this.prevDate = '';
+    this.currentDate = '';
   }
-  const hours = addZero(date.getHours());
-  const minutes = addZero(date.getMinutes());
-  const seconds = addZero(date.getSeconds());
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-const addSecond = async (date) => {
-  date.setSeconds(date.getSeconds() + 20);
-};
-
-function getTime(node, location) {
-  fetch(
-    `https://api.ipgeolocation.io/timezone?apiKey=${key}&location=${location}`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw Error('zjebauo');
-      }
-      return response.json();
-    })
-    .then((result) => {
-      const time = result.time_24.slice(0, 5);
-      if (node.name === 'Berlin') berlinPrev = time;
-      if (node.name === 'Tokio') tokioPrev = time;
-      if (node.name === 'Delhi') delhiPrev = time;
-      if (node.name === 'New York') newYorkPrev = time;
-      node.value = time;
-      node.addEventListener('change', () => {
-        changeInput(node);
+  getTime() {
+    //fetch data to change time parameter
+    fetch(
+      `https://api.ipgeolocation.io/timezone?apiKey=${key}&location=${this.name}`
+    )
+      .then((res) => {
+        if (!res.ok) throw Error('doopa');
+        return res.json();
+      })
+      .then((res) => {
+        this.date = res.date_time_txt;
+        this.prevDate = res.date_time_txt;
       });
-      return;
+  }
+  addOnChange() {
+    //adding event on the node to trigger date calculations and all that jazz
+    const node = document.querySelector(`input[name=${this.name}]`);
+    node.addEventListener('change', () => {
+      changeInput(node);
     });
+  }
+  calculateChange(val) {
+    //compare this date vals and return difference
+  }
 }
 
-inputNodes.forEach((node) => {
-  getTime(node, node.name);
-});
+// function hourToString(input) {
+//   const date = new Date(input);
+//   function addZero(num) {
+//     if (num < 10) return '0' + num;
+//     return num;
+//   }
+//   const hours = addZero(date.getHours());
+//   const minutes = addZero(date.getMinutes());
+//   const seconds = addZero(date.getSeconds());
+//   return `${hours}:${minutes}:${seconds}`;
+// }
+
+// inputNodes.forEach((node) => {
+//   const obj = new TimeZone(node.name);
+//   obj.getTime();
+//   obj.displayTime();
+//   cities.add(obj);
+// });
 
 function changeInput(node) {
   const [hours, minutes] = node.value.split(':');
