fde3c0b628df7123bacd3c7cf706e344282d9806
xanoosh <xanoosh@gmail.com>
2021-04-11 20:11:08 +0200

    time zones project - retrieve data

--- time-zones/time-zones-script.js
+++ time-zones/time-zones-script.js
@@ -12,6 +12,7 @@ let newYorkPrev;
 class TimeZone {
   constructor(name) {
     this.name = name;
+    this.node = document.querySelector(`input[name='${this.name}']`);
     this.prevDate = '';
     this.currentDate = '';
   }
@@ -33,20 +34,19 @@ class TimeZone {
       });
   }
   displayTime() {
-    const node = document.querySelector(`input[name='${this.name}']`);
-    node.value = getHourFromDate(this.currentDate);
+    this.node.value = getHourFromDate(this.currentDate);
   }
   addOnChange() {
-    //adding event on the node to trigger date calculations and all that jazz
-    const node = document.querySelector(`input[name='${this.name}']`);
-    console.log(node);
-    node.addEventListener('change', () => {
+    this.node.addEventListener('change', (e) => {
       console.log(this.name);
-      // changeHour(node);
+      //value is seconds data from input
+      this.currentDate = setDateFromInput(this.currentDate, e.target.value);
+      getDateDifference(this.prevDate, this.currentDate);
+      calculateChange(
+        this.name,
+        getDateDifference(this.prevDate, this.currentDate)
+      );
     });
-  }
-  calculateChange(val) {
-    //compare this date vals and return difference
   }
 }
 
@@ -61,13 +61,44 @@ function getHourFromDate(date) {
   return `${hours}:${minutes}`;
 }
 
-function changeHour(node) {
-  console.log(`${node.name} it targeted`);
-}
-
-//iterate through nodelist and construct objects calling methods
+// iterate through nodelist and construct objects calling methods
 inputNodes.forEach((node) => {
   const obj = new TimeZone(node.name);
   obj.getTime();
   cities.add(obj);
 });
+
+const date1 = new Date('December 17, 1995 03:24:00');
+const date2 = new Date('December 17, 1995 03:26:12');
+
+function calculateChange(name, difference) {
+  cities.forEach((city) => {
+    if (city.name !== name) {
+      city.prevDate = city.currentDate;
+      city.currentDate = setDifferentDate(city.currentDate, difference);
+      city.displayTime();
+    }
+  });
+}
+
+function getDateDifference(previous, current) {
+  const difference = (current.getTime() - previous.getTime()) / 1000;
+  console.log(difference);
+  return difference;
+}
+function setDifferentDate(date, val) {
+  return date.setSeconds(date.getSeconds() + val);
+}
+
+function setDateFromInput(date, val) {
+  const [hours, minutes] = val.split(':');
+  console.log(`previous: ${date}`);
+  date.setHours(hours);
+  date.setMinutes(minutes);
+  console.log(`new: ${date}`);
+  return date;
+}
+
+setDateFromInput(date1, '01:01');
+
+// getDateDifference(date1, date2);
